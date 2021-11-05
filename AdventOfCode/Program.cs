661577c84570aa476d185af54037f3df8172f29a
woutervanderbilt <47359805+woutervanderbilt@users.noreply.github.com>
2021-11-05 13:52:45 +0100

    Add files via upload

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Problems;
+
+namespace Euler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Problem problem = null;
+            while (problem == null)
+            {
+                Console.WriteLine("Geef nummer van probleem op:");
+                var input = Console.ReadLine();
+                int nummer;
+                if (int.TryParse(input, out nummer) && ProblemDefinitions.Problems.ContainsKey(nummer))
+                {
+                    problem = ProblemDefinitions.Problems[nummer];
+                    var sw = new Stopwatch();
+                    sw.Start();
+                    problem.ExecuteAsync().Wait();
+                    sw.Stop();
+                    Console.WriteLine($"Oplossing: {problem.Result}  ({sw.ElapsedMilliseconds} ms)");
+                    if (!string.IsNullOrWhiteSpace(problem.Result))
+                    {
+                        File.AppendAllLines(@"C:\temp\Eulerruns.txt",
+                            new[]
+                            {
+                                $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} Problem {input}: {problem.Result}"
+                            });
+                    }
+
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Ongeldig nummer");
+                }
+            }
+        }
+    }
+}
