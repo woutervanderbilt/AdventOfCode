dad81f5280b5772b0c736ad5ffb507c9453f6cd7
Wouter van der Bilt <wouter.van.der.bilt@topicus.nl>
2021-12-11 09:09:56 +0100

    dag 11

---
+++ Algorithms/Models/Grid.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Models
+{
+    public class Grid<T>
+    {
+        private IDictionary<(int, int), T> grid = new Dictionary<(int, int), T>();
+
+        public GridMember<T> this[int x, int y]
+        {
+            get { return grid.TryGetValue((x,y), out var t) ? new GridMember<T>(t) : new GridMember<T>(); }
+            set { grid[(x, y)] = value; }
+        }
+
+        public IEnumerable<(T value, int x, int y)> AllMembers()
+        {
+            foreach (var t in grid)
+            {
+                yield return (t.Value, t.Key.Item1, t.Key.Item2);
+            }
+        }
+
+
+        public IEnumerable<(T value, int x, int y)> Neighbours((int, int) location, bool includeDiagonal)
+        {
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    if (i == 0 && j == 0 || (i * j != 0 && !includeDiagonal))
+                    {
+                        continue;
+                    }
+
+                    var gridMember = this[location.Item1 + i, location.Item2 + j];
+                    if (gridMember.Found)
+                    {
+                        yield return (gridMember, location.Item1 + i, location.Item2 + j);
+                    }
+                }
+            }
+        }
+
+        public Grid<T> Copy()
+        {
+            var copy = new Grid<T>();
+            copy.grid = new Dictionary<(int, int), T>(grid);
+            return copy;
+        }
+    }
+
+    public class GridMember<T>
+    {
+        public GridMember(T value)
+        {
+            Value = value;
+            Found = true;
+        }
+
+        public GridMember()
+        {}
+
+        public T Value { get; set; }
+        public bool Found { get; set; }
+
+        public static implicit operator GridMember<T>(T t)
+        {
+            return new GridMember<T>(t);
+        }
+
+        public static implicit operator T(GridMember<T> g)
+        {
+            return g.Value;
+        }
+    }
+
+}
--- Problems/Advent/2021/Dag11.cs
+++ Problems/Advent/2021/Dag11.cs
@@ -1,14 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Algorithms.Models;
 
 namespace Problems.Advent._2021
 {
     internal class Dag11 : Problem
     {
+        private const string input = @"2478668324
+4283474125
+1663463374
+1738271323
+4285744861
+3551311515
+8574335438
+7843525826
+1366237577
+3554687226";
         public override Task ExecuteAsync()
         {
+            var grid = new Grid<int>();
+            int i = 0;
+            foreach (var line in input.Split(Environment.NewLine))
+            {
+                int j = 0;
+                foreach (var c in line)
+                {
+                    grid[i, j] = int.Parse(c.ToString());
+                    j++;
+                }
+                i++;
+            }
+
+            var gridCount = grid.AllMembers().Count();
+
+            long result = 0;
+            int count = 1;
+            while(true)
+            {
+                var flashed = Step();
+                if (count <= 100)
+                {
+                    result += flashed;
+                }
+
+                if (flashed == gridCount)
+                {
+                    break;
+                }
+
+                count++;
+            }
+
+            int Step()
+            {
+                HashSet<(int x, int y)> flashed = new HashSet<(int x, int y)>();
+                IList<(int, int)> newFlashed = new List<(int, int)>();
+                foreach (var member in grid.AllMembers())
+                {
+                    grid[member.x, member.y]++;
+                    if (member.value >= 9)
+                    {
+                        newFlashed.Add((member.x, member.y));
+                        flashed.Add((member.x, member.y));
+                    }
+                }
+                while (newFlashed .Any())
+                {
+                    IList<(int, int)> newNewFlashed = new List<(int, int)>();
+                    foreach (var member in newFlashed)
+                    {
+                        foreach (var neighbour in grid.Neighbours(member, true))
+                        {
+                            grid[neighbour.x, neighbour.y]++;
+                            if (neighbour.value >= 9 && !flashed.Contains((neighbour.x, neighbour.y)))
+                            {
+                                newNewFlashed.Add((neighbour.x, neighbour.y));
+                                flashed.Add((neighbour.x, neighbour.y));
+                            }
+                        }
+                    }
+
+                    newFlashed = newNewFlashed;
+                }
+
+                foreach (var member in flashed)
+                {
+                    grid[member.x, member.y] = 0;
+                }
+
+                return flashed.Count;
+            }
+
+            Result = $"{result}  {count}";
             return Task.CompletedTask;
         }
 
