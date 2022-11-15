d59fa5813693389bae5f9d2681facec190103ad0
Wouter van der Bilt <wouter.van.der.bilt@topicus.nl>
2022-11-15 12:36:56 +0100

    2022 templates

---
+++ Problems/Advent/2022/Dag17.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Advent._2022
+{
+    internal class Dag17 : Problem
+    {
+        public override Task ExecuteAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public override int Nummer => 202217;
+    }
+}
