d4a060d289ac3fbb36a772b17c841f2c9c0f4d25
Wouter van der Bilt <wouter.van.der.bilt@topicus.nl>
2023-12-01 09:21:09 +0100

    2023 01

---
+++ Problems/Advent/2023/Dag25.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Advent._2023
+{
+    internal class Dag25 : Problem
+    {
+        private const string input = @"";
+        public override Task ExecuteAsync()
+        {
+            foreach (var line in input.Split(Environment.NewLine))
+            {
+                
+            }
+            return Task.CompletedTask;
+        }
+
+        public override int Nummer => 202325;
+    }
+}
