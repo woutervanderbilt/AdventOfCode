7ee9f24fd45cca7a0c847d4debe53fdb4f678358
Wouter van der Bilt <wouter.van.der.bilt@topicus.nl>
2023-12-02 15:07:13 +0100

    dag 2

--- Problems/Advent/2023/Dag02.cs
+++ Problems/Advent/2023/Dag02.cs
@@ -10,11 +10,53 @@ namespace Problems.Advent._2023
     {
         public override async Task ExecuteAsync()
         {
+            int maxRed = 12;
+            int maxGreen = 13;
+            int maxBlue = 14;
+
+            long sumValidGameIds = 0;
+            long totalPower = 0;
             string input = await GetInputAsync();
             foreach (var line in input.Split(Environment.NewLine))
             {
-                
+                long minRed = 0;
+                long minBlue = 0;
+                long minGreen = 0;
+                var gameSplit = line.Split(':');
+                int gameId = int.Parse(gameSplit[0].Split(' ')[1]);
+                bool valid = true;
+                foreach (var show in gameSplit[1].Split(';'))
+                {
+                    foreach (var color in show.Split(','))
+                    {
+                        var split = color.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                        var count = int.Parse(split[0]);
+                        if (split[1] == "blue")
+                        {
+                            valid &= count <= maxBlue;
+                            minBlue = Math.Max(minBlue, count);
+                        }
+                        else if (split[1] == "red")
+                        {
+                            valid &= count <= maxRed;
+                            minRed = Math.Max(minRed, count);
+                        }
+                        else if (split[1] == "green")
+                        {
+                            valid &= count <= maxGreen;
+                            minGreen = Math.Max(minGreen, count);
+                        }
+                    }
+                }
+                if (valid)
+                {
+                    sumValidGameIds += gameId;
+                }
+
+                totalPower += minGreen * minBlue * minRed;
             }
+
+            Result = (sumValidGameIds, totalPower).ToString();
         }
 
         public override int Nummer => 202302;
