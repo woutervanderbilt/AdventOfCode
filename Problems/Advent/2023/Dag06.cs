b2a8c8faaa2082917dfb64c5e89fbecea9c3dc28
Wouter van der Bilt <wouter.van.der.bilt@topicus.nl>
2023-12-06 08:19:57 +0100

    dag 6

--- Problems/Advent/2023/Dag06.cs
+++ Problems/Advent/2023/Dag06.cs
@@ -11,18 +11,42 @@ internal class Dag06 : Problem
 {
     public override async Task ExecuteAsync()
     {
-        int[] a = [1,2,3];
-        IList<int> b = [2,3,4];
-        ICollection<int> c= [..b,2,3,455,..b,..b];
-
-
-
-
         string input = await GetInputAsync();
+        var times = new List<long>();
+        var distances = new List<long>();
         foreach (var line in input.Split(Environment.NewLine))
         {
-            
+            if (line.StartsWith("Time:"))
+            {
+                times.AddRange(line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(long.Parse));
+            }
+            else
+            {
+                distances.AddRange(line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(long.Parse));
+            }
         }
+
+        long result = 1;
+        for (int i = 0; i < times.Count; i++)
+        {
+            long count = 0;
+            for (int j = 0; j <= times[i]; j++)
+            {
+                if (j * (times[i] - j) > distances[i])
+                {
+                    count++;
+                }
+            }
+
+            result *= count;
+        }
+
+        long time = 47986698;
+        long distance = 400121310111540;
+        var x = (long)Math.Ceiling((time - Math.Sqrt(time * time - 4 * distance)) / 2);
+        long result2 = time + 1 - 2 * x;
+
+        Result = (result, result2).ToString();
     }
 
     public override int Nummer => 202306;
