9a5cc075721b84c8282938ac0e17bc3f3b765f39
Wouter van der Bilt <wouter.van.der.bilt@topicus.nl>
2023-12-22 17:41:32 +0100

    21+22

--- Problems/Advent/2023/Dag21.cs
+++ Problems/Advent/2023/Dag21.cs
@@ -3,17 +3,121 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Algorithms.Extensions;
+using Algorithms.Models;
 
 namespace Problems.Advent._2023;
 
 internal class Dag21 : Problem
 {
+    const string testinput = @"...........
+.....###.#.
+.###.##..#.
+..#.#...#..
+....#.#....
+.##..S####.
+.##..#...#.
+.......##..
+.##.#.####.
+.##..##.##.
+...........";
+
     public override async Task ExecuteAsync()
     {
         string input = await GetInputAsync();
-        foreach (var line in input.Split(Environment.NewLine))
+        var grid = Grid<char>.FromInput(input, c => c);
+        var s = grid.AllMembers().Single(m => m.value == 'S');
+        grid.CanMove = (_, m) => m.Value != '#'; 
+        var reachable = ReachableLocations((s.x, s.y), 64, grid);
+        
+
+        Console.WriteLine();
+
+
+        // oneven grids: 7697
+        // even grids: 7730
+        // magic numbers ftw
+        int target = 26501365;
+        int stepsToEdge = 26501235;
+        long maxFullGrid = 202299;
+        long odds = maxFullGrid % 2 == 0 ? (maxFullGrid + 1) * (maxFullGrid + 1) : maxFullGrid * maxFullGrid;
+        long evens = maxFullGrid % 2 == 1 ? (maxFullGrid + 1) * (maxFullGrid + 1) : maxFullGrid * maxFullGrid;
+
+        long result2 = odds * 7697 + evens * 7730;
+
+        var remaining = 130;
+        var gridTimes4 = new Grid<char>();
+        for (int y = 0; y < 262; y++)
         {
-                
+            for (int x = 0; x < 262; x++)
+            {
+                gridTimes4[x, y] = grid[x % 131, y % 131];
+            }
+        }
+        gridTimes4.CanMove = (_, m) => m.Value != '#';
+
+        var r = ReachableLocations((130, 65), remaining, grid);
+        var l = ReachableLocations((0, 65), remaining, grid);
+        var d = ReachableLocations((65, 0), remaining, grid);
+        var u = ReachableLocations((65, 130), remaining, grid);
+
+        var r1 = ReachableLocations((261, 65), remaining, gridTimes4);
+        var l1 = ReachableLocations((0, 65), remaining, gridTimes4);
+        var d1 = ReachableLocations((65, 0), remaining, gridTimes4);
+        var u1 = ReachableLocations((65, 261), remaining, gridTimes4);
+        var r2 = ReachableLocations((261, 196), remaining, gridTimes4);
+        var l2 = ReachableLocations((0, 196), remaining, gridTimes4);
+        var d2 = ReachableLocations((196, 0), remaining, gridTimes4);
+        var u2 = ReachableLocations((196, 261), remaining, gridTimes4);
+
+
+
+        result2 += r.Count + l.Count + d.Count + u.Count +
+                   maxFullGrid * (new HashSet<(int, int)>(r.Concat(d)).Count +
+                                  new HashSet<(int, int)>(r.Concat(u)).Count +
+                                  new HashSet<(int, int)>(l.Concat(d)).Count +
+                                  new HashSet<(int, int)>(l.Concat(u)).Count);
+        result2 += (maxFullGrid + 1) *
+                   (new HashSet<(int,int)>(d2.Where(m => m is { Item1: < 131, Item2: < 131 })
+                       .Concat(r1.Where(m => m is { Item1: > 130, Item2: > 130 })
+                           .Select(m => (m.Item1 - 131, m.Item2 - 131)))).Count +
+
+                    new HashSet<(int, int)>(d1.Where(m => m is { Item1: > 130, Item2: < 131 })
+                        .Select(m => (m.Item1 - 131, m.Item2))
+                        .Concat(l1.Where(m => m is { Item1: < 131, Item2: > 130 })
+                            .Select(m => (m.Item1, m.Item2 - 131)))).Count +
+
+                    new HashSet<(int, int)>(l2.Where(m => m is { Item1: < 131, Item2: < 131 })
+                        .Concat(u1.Where(m => m is { Item1: > 130, Item2: > 130 })
+                            .Select(m => (m.Item1 - 131, m.Item2 - 131)))).Count +
+
+                    new HashSet<(int, int)>(r2.Where(m => m is { Item1: > 130, Item2: < 131 })
+                        .Select(m => (m.Item1 - 131, m.Item2))
+                        .Concat(u2.Where(m => m is { Item1: < 131, Item2: > 130 })
+                            .Select(m => (m.Item1, m.Item2 - 131)))).Count);
+
+
+
+        Result = (reachable.Count, result2).ToString();
+
+        HashSet<(int, int)> ReachableLocations((int x, int y) l, int steps, Grid<char> gridToUse)
+        {
+            HashSet<(int, int)> reachable = new HashSet<(int, int)> { l };
+            for (int i = 1; i <= steps; i++)
+            {
+                var newReachable = new HashSet<(int, int)>();
+                foreach (var location in reachable)
+                {
+                    foreach (var neighbour in gridToUse.Neighbours(location, false))
+                    {
+                        newReachable.Add(neighbour.Location);
+                    }
+                }
+
+                reachable = newReachable;
+            }
+
+            return reachable;
         }
     }
 
--- Problems/Advent/2023/Dag22.cs
+++ Problems/Advent/2023/Dag22.cs
@@ -3,19 +3,131 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Algorithms.Extensions;
 
 namespace Problems.Advent._2023;
 
 internal class Dag22 : Problem
 {
+    private const string testinput = @"1,0,1~1,2,1
+0,0,2~2,0,2
+0,2,3~2,2,3
+0,0,4~0,2,4
+2,0,5~2,2,5
+0,1,6~2,1,6
+1,1,8~1,1,9";
+
     public override async Task ExecuteAsync()
     {
+        IList<Brick> bricks = new List<Brick>();
         string input = await GetInputAsync();
-        foreach (var line in input.Split(Environment.NewLine))
+        foreach (var (line, index) in input.Split(Environment.NewLine).Indexed())
         {
-                
+            var split = line.Split('~');
+            var left = split[0].Split(',');
+            var right = split[1].Split(',');
+            var minX = int.Parse(left[0]);
+            var minY = int.Parse(left[1]);
+            var minZ = int.Parse(left[2]);
+            var maxX = int.Parse(right[0]);
+            var maxY = int.Parse(right[1]);
+            var maxZ = int.Parse(right[2]);
+            if (minX > maxX)
+            {
+                (minX, maxX) = (maxX, minX);
+            }
+            if (minY > maxY)
+            {
+                (minY, maxY) = (maxY, minY);
+            }
+            if (minZ > maxZ)
+            {
+                (minZ, maxZ) = (maxZ, minZ);
+            }
+            bricks.Add(new Brick(minX, minY, minZ, maxX, maxY, maxZ, index));
         }
+
+        IDictionary<int, IList<Brick>> fixedBricksByLayer = new Dictionary<int, IList<Brick>>();
+        IDictionary<int, IList<int>> supportedByDict = new Dictionary<int, IList<int>>();
+        IDictionary<int, IList<int>> supportingDict = new Dictionary<int, IList<int>>();
+
+        foreach (var brick in bricks.ToList().OrderBy(b => b.MinZ))
+        {
+            IList<int> supportedBy = new List<int>();
+            IList<int> supporting = new List<int>();
+            supportedByDict[brick.Index] = supportedBy;
+            supportingDict[brick.Index] = supporting;
+            var dz = brick.MaxZ - brick.MinZ;
+            int z = brick.MinZ - 1;
+            for (; z > 0; z--)
+            {
+                if (fixedBricksByLayer.TryGetValue(z, out var fixedBricksAtZ))
+                {
+                    foreach (var supportingBrick in fixedBricksAtZ)
+                    {
+                        if (brick.MaxX >= supportingBrick.MinX &&
+                            brick.MinX <= supportingBrick.MaxX &&
+                            brick.MaxY >= supportingBrick.MinY &&
+                            brick.MinY <= supportingBrick.MaxY)
+                        {
+                            supportedBy.Add(supportingBrick.Index);
+                            supportingDict[supportingBrick.Index].Add(brick.Index);
+                        }
+                    }
+                }
+
+                if (supportedBy.Any())
+                {
+                    break;
+                }
+            }
+
+            var newBrick = brick with { MinZ = z + 1, MaxZ = z + dz + 1 };
+            bricks[brick.Index] = newBrick;
+            if(fixedBricksByLayer.TryGetValue(z + dz + 1, out var fixedList))
+            {
+                fixedList.Add(newBrick);
+            }
+            else
+            {
+                fixedBricksByLayer[z + dz + 1] = new List<Brick> { newBrick };
+            }
+        }
+
+        HashSet<int> supportingBricks = new HashSet<int>(supportedByDict.Values.Where(s => s.Count == 1).Select(s => s[0]));
+
+        long result2 = 0;
+        foreach (var brick in bricks)
+        {
+            HashSet<int> falling = new HashSet<int> { brick.Index };
+            HashSet<int> fallen = new HashSet<int> { brick.Index };
+            while (falling.Any())
+            {
+                HashSet<int> newFalling = new HashSet<int>();
+                foreach (var b in falling)
+                {
+                    foreach (var c in supportingDict[b])
+                    {
+                        if (supportedByDict[c].All(s => fallen.Contains(s)))
+                        {
+                            if (fallen.Add(c))
+                            {
+                                newFalling.Add(c);
+                                result2++;
+                            }
+                        }
+                    }
+                }
+
+                falling = newFalling;
+            }
+
+        }
+
+        Result = ((bricks.Count - supportingBricks.Count), result2).ToString();
     }
+
+    record Brick(int MinX, int MinY, int MinZ, int MaxX, int MaxY, int MaxZ, int Index);
 
     public override int Nummer => 202322;
 }
