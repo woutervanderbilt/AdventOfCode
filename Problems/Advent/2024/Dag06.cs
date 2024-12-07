4718c5e2f08d704c917ab161a636975419e71fe0
WOUTER\woute <wouter.van.der.bilt@topicus.nl>
2024-12-07 09:56:19 +0100

    dag 7

--- Problems/Advent/2024/Dag06.cs
+++ Problems/Advent/2024/Dag06.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Algorithms.Models;
 
 namespace Problems.Advent._2024;
 
@@ -7,12 +11,58 @@ internal class Dag06 : Problem
 {
     public override async Task ExecuteAsync()
     {
-
-        foreach (var line in Input.Split(Environment.NewLine))
+        var grid = Grid<char>.FromInput(Input, c => c);
+        int result = 0;
+        foreach (var member in grid.AllGridMembers().Where(m => m.Value == '.').ToList())
         {
+            var location = grid.AllGridMembers().Single(g => g.Value == '^');
+            HashSet<(int, int, GridDirection direction)> path = [];
+            var direction = GridDirection.North;
 
+            grid[member.X, member.Y] = '#';
+            while (location.Found)
+            {
+                if (!path.Add((location.Location.Item1, location.Location.Item2, direction)))
+                {
+                    result++;
+                    break;
+                }
+
+                var next = grid.MoveInDirection(location.Location, direction);
+                while (next.Found && next.Value == '#')
+                {
+                    direction = direction switch
+                    {
+                        GridDirection.East => GridDirection.South,
+                        GridDirection.South => GridDirection.West,
+                        GridDirection.West => GridDirection.North,
+                        GridDirection.North => GridDirection.East,
+                        _ => throw new ArgumentOutOfRangeException()
+                    };
+                    next = grid.MoveInDirection(location.Location, direction);
+                }
+
+                location = next;
+            }
+
+            grid[member.X, member.Y] = '.';
         }
+
+        Result = result.ToString();
     }
+
+    protected override string TestInput => @"....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...";
+
+    override protected bool UseTestInput => false; 
 
     public override int Nummer => 202406;
 }
--- Problems/Advent/2024/Dag07.cs
+++ Problems/Advent/2024/Dag07.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Algorithms.Extensions;
 
 namespace Problems.Advent._2024;
 
@@ -7,12 +11,77 @@ internal class Dag07 : Problem
 {
     public override async Task ExecuteAsync()
     {
-
+        IList<long> targets = [];
+        IList<IList<long>> numbers = [];
         foreach (var line in Input.Split(Environment.NewLine))
         {
-
+            var split = line.Split(": ");
+            targets.Add(long.Parse(split[0]));
+            numbers.Add(split[1].Split(' ').Select(long.Parse).ToList());
         }
+
+        long result = 0;
+        foreach (var (target, index) in targets.Indexed())
+        {
+            var numbersForTarget = numbers[index];
+            if (AddDigit(numbersForTarget[0], 0))
+            {
+                result += target;
+            }
+
+            bool AddDigit(long current, int numberIndex)
+            {
+                if (numberIndex == numbersForTarget.Count - 1)
+                {
+                    return current == target;
+                }
+                var number = numbersForTarget[numberIndex + 1];
+                if (current * number <= target)
+                {
+                    if (AddDigit(current * number, numberIndex + 1))
+                    {
+                        return true;
+                    }
+                }
+                if (current + number <= target)
+                {
+                    if (AddDigit(current + number, numberIndex + 1))
+                    {
+                        return true;
+                    }
+                }
+
+                var shift = current;
+                for (int i = 1; i <= number.ToString().Length; i++)
+                {
+                    shift *= 10;
+                }
+                if (shift + number <= target)
+                {
+                    if (AddDigit(shift + number, numberIndex + 1))
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        }
+
+        Result = result.ToString();
     }
+
+    protected override bool UseTestInput => false;
+
+    protected override string TestInput => @"190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20";
 
     public override int Nummer => 202407;
 }
