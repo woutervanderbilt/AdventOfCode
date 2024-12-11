cd490ac647e01e4ef30ec94465eb1f3b0c3cbddc
WOUTER\woute <wouter.van.der.bilt@topicus.nl>
2024-12-11 08:53:05 +0100

    dag 11

--- Problems/Advent/2024/Dag11.cs
+++ Problems/Advent/2024/Dag11.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Algorithms.Models;
 
 namespace Problems.Advent._2024;
 
@@ -7,12 +10,55 @@ internal class Dag11 : Problem
 {
     public override async Task ExecuteAsync()
     {
-
-        foreach (var line in Input.Split(Environment.NewLine))
+        CounterLong<long> counter = new();
+        foreach (var stone in Input.Split(' ').Select(long.Parse))
         {
+            counter[stone]++;
+        }
 
+        long result1 = 0;
+        for (int i = 1; i <= 75; i++)
+        {
+            var newCounter = new CounterLong<long>();
+            foreach (var stone in counter.Keys)
+            {
+                var count = counter[stone];
+                foreach (var newStone in Blink(stone))
+                {
+                    newCounter[newStone] += count;
+                }
+            }
+            counter = newCounter;
+            if (i == 25)
+            {
+                result1 = counter.Values.Sum();
+            }
+        }
+        Result = (result1, counter.Values.Sum()).ToString();
+
+
+        IEnumerable<long> Blink(long stone)
+        {
+            if (stone == 0)
+            {
+                yield return 1;
+                yield break;
+            }
+            var stoneAsString = stone.ToString();
+            if (stoneAsString.Length % 2 == 0)
+            {
+                yield return long.Parse(stoneAsString.Substring(0, stoneAsString.Length / 2));
+                yield return long.Parse(stoneAsString.Substring(stoneAsString.Length / 2, stoneAsString.Length / 2));
+            }
+            else
+            {
+                yield return stone * 2024;
+            }
         }
     }
+
+    protected override bool UseTestInput => false;
+    protected override string TestInput => @"125 17";
 
     public override int Nummer => 202411;
 }
